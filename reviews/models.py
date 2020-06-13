9b34098439163260c86331216f7f325741e5aefa
양혜빈 <skdisk7368@naver.com>
2020-06-13 22:31:12 +0900

    update css

--- reviews/models.py
+++ reviews/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 from movies.models import Movie
 
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_reviews')
-    content = models.CharField(max_length=500)
+    content = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='my_reviews')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
 
 def image_path(instance, filename):
-    return f'posts/{instance.post.content}/{filename}'
+    return f'reviews/{instance.review.content}/{filename}'
 
 class Image(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_images')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='review_images')
     image_files = ProcessedImageField(
             upload_to = image_path,
             processors = [ResizeToFill(600, 600)],
