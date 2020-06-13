5853c51b3cbd4c29202260ad21616365e3f7075c
양혜빈 <skdisk7368@naver.com>
2020-06-13 22:31:12 +0900

    update css

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-from .models import Review, Comment
+from .models import Review, Comment, Image
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['content', ]
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image_files', ]
+        
+ImageFormSet = forms.inlineformset_factory(Review, Image, form=ImageForm, extra=3)
 
 class CommentForm(forms.ModelForm):
     class Meta:
--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET, require_POST, require_http_methods 
+from django.db import transaction
 from movies.models import Movie
-from .models import Review
-from .forms import ReviewForm
+from .models import Review, Comment, Image
+from .forms import ReviewForm, CommentForm, ImageFormSet
 
 @require_GET
+@login_required
 def movie_review_list(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     reviews = movie.movie_reviews.all()
@@ -14,23 +17,32 @@ def movie_review_list(request, movie_pk):
     }
     return render(request, 'reviews/review_list.html', context)
 
+@login_required
 @require_http_methods(['GET', 'POST'])
 def movie_create_review(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     if request.method == 'POST':
         form = ReviewForm(request.POST)
-        if form.is_valid():
+        image_formset = ImageFormSet(request.POST, request.FILES)
+        if form.is_valid() and image_formset.is_valid():
             review = form.save(commit=False)
+            review.author = request.user
             review.movie = movie
-            review.save()
-            return redirect('reviews:movie_review_list', movie.pk)
+            with transaction.atomic():
+                review.save()
+                image_formset.instance = review
+                image_formset.save()
+                return redirect('reviews:movie_review_list', movie.pk)
     else:
         form =ReviewForm()
+        image_formset = ImageFormSet()
     context = {
-        'form': form
+        'form': form,
+        'image_formset': image_formset,
     }
     return render(request, 'reviews/review_form.html', context)
 
+@login_required
 @require_http_methods(['GET', 'POST'])
 def movie_update_review(request, movie_pk, review_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
@@ -47,6 +59,7 @@ def movie_update_review(request, movie_pk, review_pk):
     return render(request, 'reviews/review_form.html', context)
 
 @require_POST
+@login_required
 def movie_delete_review(request, movie_pk, review_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     review = get_object_or_404(Review, pk=review_pk)
