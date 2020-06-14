472a1b4369e63e73b97102f32871df3f1f9ed736
양혜빈 <skdisk7368@naver.com>
2020-06-15 01:38:53 +0900

    modify css

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Genre(models.Model):
     name = models.CharField(max_length=200)
@@ -15,4 +16,5 @@ class Movie(models.Model):
     original_language = models.CharField(max_length=200)
     poster_path = models.CharField(max_length=200)
     backdrop_path = models.CharField(max_length=200)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
     genres = models.ManyToManyField(Genre, related_name='genre_movies')
