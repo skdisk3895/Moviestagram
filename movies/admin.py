25f6c21558a06a01e0ec8eb0fac2223bac6daf2a
김가람 <ursopretty93@naver.com>
2020-06-17 14:39:29 +0900

    modify score

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Genre, Movie
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_display_links = ['title']
