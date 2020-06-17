9bc7ff7327281ce4ce26a0432537edd333f25d4d
김가람 <ursopretty93@naver.com>
2020-06-17 17:36:36 +0900

    admin movie create

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_http_methods
 from .models import Movie
 
 @require_GET
@@ -12,3 +12,21 @@ def movie_list(request):
         'movies': movies,
     }
     return render(request, 'movies/movie_list.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def movie_create(request):
+    if request.user.username != 'admin':
+        return redirect('home:home')
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Movie.movie_search(title, 1)
+        return redirect('home:home')
+    else:
+        context = {}
+        return render(request, 'movies/movie_create.html', context)
+    
+    
+
+
+
