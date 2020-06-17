d6eaf944cefafa4857883dae088c1a7f827474ae
김가람 <ursopretty93@naver.com>
2020-06-17 17:36:36 +0900

    admin movie create

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+import requests
 
 class Genre(models.Model):
     name = models.CharField(max_length=200)
@@ -18,3 +19,34 @@ class Movie(models.Model):
     backdrop_path = models.CharField(max_length=200)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
     genres = models.ManyToManyField(Genre, related_name='genre_movies')
+
+    @classmethod
+    def movie_search(cls, title, page):
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        for n in range(page):
+            movies = requests.get(
+                f'https://api.themoviedb.org/3/search/movie?api_key=f9cad3d60c9f4660d0f5186548616cf1&query='+ title
+            ).json().get('results')
+            # print(movies)
+            for movie in movies:
+                    if  Movie.objects.filter(pk=movie.get('id')).exists():
+                        continue
+                    m, created = cls.objects.get_or_create(
+                        id=movie.get('id'),
+                        title=movie.get('title'),
+                        original_title=movie.get('original_title'),
+                        release_date=movie.get('release_date'),
+                        popularity=movie.get('popularity'),
+                        vote_count=movie.get('vote_count'),
+                        vote_average=movie.get('vote_average'),
+                        adult=movie.get('adult'),
+                        overview=movie.get('overview'),
+                        original_language=movie.get('original_language'),
+                        poster_path=movie.get('poster_path'),
+                        backdrop_path=movie.get('backdrop_path') if movie.get('backdrop_path') else '',
+                    )
+                    if created:
+                        for genre_id in movie.get('genre_ids'):
+                            g = Genre.objects.get(pk=genre_id)
+                            m.genres.add(g)
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
+    path('admin/create/', views.movie_create, name='movie_create'),
 ]
